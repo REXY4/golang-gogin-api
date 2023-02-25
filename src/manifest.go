9fd7d5188f3a6d52ee0cc83be006315ca1495ce0
REXY4 <muhammadrzki96@gmail.com>
2023-02-26 04:19:50 +0700

    complete set

--- src/manifest.go
+++ src/manifest.go
@@ -6,14 +6,14 @@ import (
 	"github.com/REXY4/golang-gogin-api/config"
 )
 
-func setup() {
+func setup(config string) {
+	Server(config)
+}
+
+func Start() {
 	configStr, err := config.Configs()
 	if err != nil {
 		log.Panic(err)
 	}
-	Server(configStr)
-}
-
-func Start() {
-	setup()
+	setup(configStr)
 }
